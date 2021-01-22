0c212bc10795f63904314aca6ce7a5fdc91f84e2
Mark Crowther <mark.crowther@bjss.com>
2021-01-22 08:07:42 +0000

    Added query 2

---
+++ telusko-tutorial/query-2.sql
@@ -0,0 +1,28 @@
+-- SQL to query the data sets from the tutorial by Telesko
+-- https://www.youtube.com/watch?v=MdjmTSTQShc
+-- Written by me, feel free to use as you wish.
+-- !! Please note the tweaked column names and emp_name data when running scripts in the tutorial !!
+-- ---------------------------------------------------------
+
+-- Written for MySQL using https://www.mysql.com/products/workbench/ (NOT Oracle as in the tutorial)
+-- Tweak any failures for your particular version of SQL
+-- Each section is written to run on its own in sequence for simplicity of understanding
+
+-- ---------------------------------------------------------
+
+-- Query 2: Find the Clerk with a salary of 5000 (Furedi)
+
+-- Simple query
+select emp_no as 'Employee No.', emp_name as 'Employee Name'
+from employee
+where designation = 'Clerk'
+	and salary = 5000;
+
+-- Nested sub-query
+select emp_no as 'Employee No.', emp_name as 'Employee Name'
+from employee
+where (designation, salary) in (
+                        select designation, salary
+						from employee
+						where designation = 'Clerk'
+							and salary = 5000);
