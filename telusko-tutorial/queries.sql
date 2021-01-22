ee55ecb4809aeabc25a0a09c79fba65d6fa783ff
Mark Crowther <mark.crowther@bjss.com>
2021-01-22 07:44:59 +0000

    Updatesd queries and readme

--- telusko-tutorial/queries.sql
+++ telusko-tutorial/queries.sql
@@ -1,10 +1,10 @@
--- SQL to create the data sets from the tutorial by Telesko
+-- SQL to query the data sets from the tutorial by Telesko
 -- https://www.youtube.com/watch?v=MdjmTSTQShc
 -- Written by me, feel free to use as you wish.
 -- !! Please note the tweaked column names and emp_name data when running scripts in the tutorial !!
 -- ---------------------------------------------------------
 
--- Written for MySQL using https://www.beekeeperstudio.io/ and https://www.mysql.com/products/workbench/
+-- Written for MySQL using https://www.mysql.com/products/workbench/ (NOT Oracle as in the tutorial)
 -- Tweak any failures for your particular version of SQL
 -- Each section is written to run on its own in sequence for simplicity of understanding
 
@@ -20,3 +20,13 @@ where designation = 'Manager'
       select max(salary)
       from employee
       where designation = 'Manager');
+
+-- Nested Query 2
+-- This query returns the same result as above (5 Upadhyaya)
+-- Here we use an 'in', but instead of a list of given values, we derive them via the subquery
+select emp_no as 'Employee No.', emp_name as 'Employee Name'
+from employee
+where (designation, salary) in (
+                select designation, max(salary)
+                from employee
+                where designation = 'Manager');
---
+++ telusko-tutorial/readme.md
@@ -0,0 +1,16 @@
+Keywords: sql, sub queries
+
+# SQL Tutorial by Telusko
+
+The original tutorial was demonstrated in Oracle SQL, 
+here I've converted the script to work with MySQL using MySQL Workbench (https://www.mysql.com/products/workbench/).
+
+Tweak any failures for your particular version of SQL.
+
+### Contents of the repo
+* build-the-data.sql : This builds the DB, tables and adds the data
+* queries.sql: This file contains the example queries and query variants for the data
+
+*Query set*
+ 1) Amongst managers, who gets the highest salary?
+ 2) Get the details of the Clerk earning 5000
