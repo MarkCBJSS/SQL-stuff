7133aec0c3fd4454c649165bb72adc6deaf187d6
Mark Crowther <mark.crowther@bjss.com>
2021-02-15 21:21:30 +0000

    Modifed tutorial to work with MySQL

---
+++ telusko-tutorial/query-4.sql
@@ -0,0 +1,25 @@
+-- SQL to query the data sets from the tutorial by Telesko
+-- https://www.youtube.com/watch?v=MdjmTSTQShc
+-- Written by me, feel free to use as you wish.
+-- !! Please note the tweaked column names and emp_name data when running scripts in the tutorial !!
+-- ---------------------------------------------------------
+
+-- Written for MySQL using https://www.mysql.com/products/workbench/ (NOT Oracle as in the tutorial)
+-- Tweak any failures for your particular version of SQL
+-- Each section is written to run on its own in sequence for simplicity of understanding
+
+-- ---------------------------------------------------------
+
+-- Query 4: Which designation has most number of employees
+
+-- Every derived table must have its own alias
+select designation as "Designation", count(*) as "Count"
+from employee
+group by designation
+having count(*) = (
+      select max(Total) as "Total"
+		from (
+      		select count(*) as Total
+      		from employee
+         	group by designation) as Results
+		);
